294792e1a0c29618eddaf886692bc1f12ccd039d
Ari Agafonova <agafonova_arina@icloud.com>
2026-03-29 14:27:02 +0200

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Арина Агафонова on 29.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
